c6dfa50e62ebf6bd55abb082b7839c8bd5930722
Sean Sundberg <seansund@us.ibm.com>
2022-02-18 08:07:03 -0600

    Provision two subnets in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-vpc-subnet.tf
+++ test/stages/stage2-vpc-subnet.tf
@@ -5,8 +5,29 @@ module "dev_vpc_subnet" {
   provision_igw = var.provision
   provision_ngw = var.provision
   prefix_name = var.prefix_name
-  private_subnet_cidr             = var.private_subnet_cidr
-  public_subnet_cidr              = var.public_subnet_cidr
+  private_subnet_cidr             = ["10.0.125.0/24"]
+  public_subnet_cidr              = ["10.0.0.0/20"]
+  availability_zones              = var.availability_zones
+  map_customer_owned_ip_on_launch = false
+  map_public_ip_on_launch         = false
+  tags                            = var.tags
+  public_subnet_tags              = var.public_subnet_tags
+  private_subnet_tags             = var.private_subnet_tags
+  acl_rules_pub_in = var.acl_rules_pub_in
+  acl_rules_pub_out = var.acl_rules_pub_out
+  acl_rules_pri_in= var.acl_rules_pri_in
+  acl_rules_pri_out = var.acl_rules_pri_out
+}
+
+module "dev_vpc_subnet" {
+  source                          = "./module"
+  vpc_id                          = module.dev_vpc.vpc_id
+  provision=var.provision
+  provision_igw = var.provision
+  provision_ngw = var.provision
+  prefix_name = var.prefix_name
+  private_subnet_cidr             = ["10.0.126.0/24"]
+  public_subnet_cidr              = ["10.0.16.0/20"]
   availability_zones              = var.availability_zones
   map_customer_owned_ip_on_launch = false
   map_public_ip_on_launch         = false
