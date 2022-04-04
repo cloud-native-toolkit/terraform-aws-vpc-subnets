2e3d4098b618ec03b8f6d38e12d65b54366decc0
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2022-04-04 09:32:15 +0530

    updated module yml and output ids
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,15 @@ output "subnet_ids" {
   value = data.aws_subnets.subnet_ids.ids
 }
 
+output "ids" {
+  #value = data.aws_subnets.subnet_ids.ids
+  value= aws_subnet.subnet.*.id
+}
+
 output "subnet_count" {
+value = local.num_of_sn_cidrs
+}
+
+output "count" {
 value = local.num_of_sn_cidrs
 }
