a08f27e358eedaa3f21905720bad0b8e74b5c490
indirakalagara <indira.kalagara@in.ibm.com>
2022-04-27 09:52:08 +0530

    updated workflow actions & outputs (#17)
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    Co-authored-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- outputs.tf
+++ outputs.tf
@@ -2,21 +2,28 @@ output "vpc_id" {
   value  = local.vpc_id
 }
 
-output "subnet_ids" {
-  #value = data.aws_subnets.subnet_ids.ids
-  value= aws_subnet.subnet.*.id
-}
 
 output "ids" {
   #value = data.aws_subnets.subnet_ids.ids
   value= aws_subnet.subnet.*.id
 }
+output "count" {
+value = local.num_of_sn_cidrs
+}
 
+output "route_table_ids" {
+  #value = data.aws_subnets.subnet_ids.ids
+  value = aws_route_table.subnet_rt.*.id
+}
+
+#Remove below 2 outputs after testing all archs.  Reason- Having subnet as prefix.  
+#Currently keeping it for making sure other modules doesn't break
+output "subnet_ids" {
+  #value = data.aws_subnets.subnet_ids.ids
+  value= aws_subnet.subnet.*.id
+}
 
 output "subnet_count" {
 value = local.num_of_sn_cidrs
 }
 
-output "count" {
-value = local.num_of_sn_cidrs
-}
